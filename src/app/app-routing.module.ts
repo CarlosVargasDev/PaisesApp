510d0a9f0c0af7b770e8855836dfd5f1defb0158
Carlos Vargas <Carlos.Vargas.Dev@gmail.com>
2022-01-04 17:49:24 -0600

    Sistema de rutas agregado

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import {NgModule, } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Componentes
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+
+const routes: Routes = [
+    { // Ruta principal: porPaisComponent
+        path:'',
+        component: PorPaisComponent,
+        pathMatch: 'full'
+    },
+    { // ruta: PorRegionComponent
+        path:'region',
+        component: PorRegionComponent,  
+    },
+    { // ruta: PorCapitalComponent
+        path:'capital',
+        component: PorCapitalComponent,
+    },
+    { // ruta: VerPaisComponent
+        path:'pais/:id',
+        component: VerPaisComponent,
+    },
+    { // ruta por defecto
+        path:'**',
+        redirectTo: ''
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{
+
+}
+
